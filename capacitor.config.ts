37410ffffdf2415ab2f12b414e9ba55f00ca16ac
Yoshiaki Itakura <itathirdchildnegi@gmail.com>
2024-01-02 12:19:51 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_angular17_tutorial',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
